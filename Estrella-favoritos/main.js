3a883b9ca07fe6ca9030a4fc91b368888970e9fb
Daniel Valdivieso-M <dvaldi7@gmail.com>
2024-10-15 23:46:14 +0200

    finished

--- Estrella-favoritos/main.js
+++ Estrella-favoritos/main.js
@@ -1,13 +1,15 @@
 window.addEventListener("DOMContentLoaded", () => {
 
-    let grey = document.querySelector(".grey");
-    let yellow = document.querySelector(".yellow");
+    let grey = document.querySelector(".main-star--grey");
+    let yellow = document.querySelector(".main-star--yellow");
 
     grey.addEventListener("click", () =>{
         yellow.classList.add("visible");
+        grey.classList.add("yellow-color");
     });
 
     yellow.addEventListener("click", () =>{
         yellow.classList.remove("visible");
+        grey.classList.remove("yellow-color");
     });
 });
