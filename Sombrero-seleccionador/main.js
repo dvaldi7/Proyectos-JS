cf3dfe63b8035fb591b87cc2c0bdaf7dfcab0455
Daniel Valdivieso-M <dvaldi7@gmail.com>
2024-10-24 01:56:58 +0200

    added sombrero-seleccionador

---
+++ Sombrero-seleccionador/main.js
@@ -0,0 +1,24 @@
+/*
+ * EJERCICIO:
+ * Cada 1 de septiembre, el Hogwarts Express parte hacia la escuela
+ * de programación de Hogwarts para magos y brujas del código.
+ * En ella, su famoso sombrero seleccionador ayuda a los programadores
+ * a encontrar su camino...
+ * Desarrolla un programa que simule el comportamiento del sombrero.
+ * Requisitos:
+ * 1. El sombrero realizará 10 preguntas para determinar la casa del alumno.
+ * 2. Deben existir 4 casas. Por ejemplo: Frontend, Backend, Mobile y Data.
+ *    (Puedes elegir las que quieras)
+ * Acciones:
+ * 1. Crea un programa que solicite el nombre del alumno y realice 10
+ *    preguntas, con cuatro posibles respuestas cada una.
+ * 2. Cada respuesta asigna puntos a cada una de las casas (a tu elección).
+ * 3. Una vez finalizado, el sombrero indica el nombre del alumno 
+ *    y a qué casa pertenecerá (resuelve el posible empate de manera aleatoria,
+ *    pero indicándole al alumno que la decisión ha sido complicada).
+ */
+
+let casas = ["Griffindor", "Slyhering", "Hufflepuff", "Ravenclaw"];
+
+let nombre = prompt("Hola, Bienvenido a Hogwarts, soy el sombrero seleccionador, cuál es tu nombre? ");
+console.log("Hola " + nombre + ", un placer conocerte. Veamos a que casa puedo asignarte... Responde con sinceridad.");
